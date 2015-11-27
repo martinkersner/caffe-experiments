bc96f674735d7ba692fb63336c30fdbea24b0ae4
Martin Keršner <m.kersner@gmail.com>
2015-11-27 10:15:37 +0900

    refactoring of main

--- pooling_layer/pooling_layer.py
+++ pooling_layer/pooling_layer.py
@@ -9,23 +9,15 @@ import Image
 
 sys.path.insert(0, "../") # add tools.py
 import paths as p
-import tools
+import tools as tl
 
-tools.load_caffe()
+tl.load_caffe()
 import caffe
+
+layer_name = "pool"
 
 caffe.set_mode_gpu()
 net = caffe.Net(p.net_definition, caffe.TEST)
-
-img = np.array(Image.open(p.img_input_path))
-
-# preprocess image
-img_input = img[np.newaxis, np.newaxis, :, :]
-net.blobs['data'].reshape(*img_input.shape)
-net.blobs['data'].data[...] = img_input
-
-# apply filter
-net.forward()
-
-img_output = tools.normalize_to_range(net.blobs['pool'].data[0, 0], 255)
-tools.save_grayscale_image_from_arary(img_output, p.img_output_path)
+tl.preprocess_image(net, p.img_input_path)
+img_output = tl.apply_filter(net, layer_name)
+tl.save_grayscale_image_from_arary(img_output, p.img_output_path)
--- tools.py
+++ tools.py
@@ -8,6 +8,10 @@ import sys
 import Image
 import numpy as np
 
+def load_caffe():
+    caffe_root = os.environ['CAFFE_ROOT']
+    sys.path.insert(0, caffe_root + '/python')
+
 def save_rgb_image_from_arary(img_arr, img_name):
     im = Image.fromarray(img_arr)
     im.convert('RGB').save(img_name)
@@ -17,13 +21,24 @@ def save_grayscale_image_from_arary(img_arr, img_name):
     im = Image.fromarray(img_arr)
     im.save(img_name)
 
-def load_caffe():
-    caffe_root = os.environ['CAFFE_ROOT']
-    sys.path.insert(0, caffe_root + '/python')
-
 def normalize_to_range(img, max_thresh):
     img_min = np.min(img),
     img_max = np.max(img)
     diff = img_max - img_min
 
     return ((img - img_min) / diff) * max_thresh
+
+def preprocess_image(net, img_path):
+    img = np.array(Image.open(img_path))
+
+    # preprocess image
+    img_input = img[np.newaxis, np.newaxis, :, :]
+    net.blobs['data'].reshape(*img_input.shape)
+    net.blobs['data'].data[...] = img_input
+
+def apply_filter(net, layer_name):
+    net.forward()
+    img_output = net.blobs[layer_name].data[0, 0]
+    img_norm = normalize_to_range(img_output, 255)
+
+    return img_output
