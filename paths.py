66930738e3378a53042e5f1b288276265b2d8932
Martin Keršner <m.kersner@gmail.com>
2015-11-27 09:57:23 +0900

    separate paths to input/output images and net

---
+++ paths.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+# Martin Kersner, m.kersner@gmail.com
+# 2015/11/27
+
+img_input_path  = "../images/lena_gray.png"
+img_output_path = "lena_out.png"
+net_definition  = "net.prototxt"
--- pooling_layer/pooling_layer.py
+++ pooling_layer/pooling_layer.py
@@ -6,19 +6,18 @@
 import sys
 import numpy as np
 import Image
-sys.path.insert(0, "../")
+
+sys.path.insert(0, "../") # add tools.py
+import paths as p
 import tools
+
 tools.load_caffe()
 import caffe
 
-img_input_path = '../images/lena_gray.png'
-img_output_path = 'lena_out.png'
-net_definition = 'net.prototxt'
-
 caffe.set_mode_gpu()
-net = caffe.Net(net_definition, caffe.TEST)
+net = caffe.Net(p.net_definition, caffe.TEST)
 
-img = np.array(Image.open(img_input_path))
+img = np.array(Image.open(p.img_input_path))
 
 # preprocess image
 img_input = img[np.newaxis, np.newaxis, :, :]
@@ -29,4 +28,4 @@ net.blobs['data'].data[...] = img_input
 net.forward()
 
 img_output = tools.normalize_to_range(net.blobs['pool'].data[0, 0], 255)
-tools.save_grayscale_image_from_arary(img_output, img_output_path)
+tools.save_grayscale_image_from_arary(img_output, p.img_output_path)
